280f1cd435954c2f064a693f7cb611792d4e6094
paultis <11512940+paultis@users.noreply.github.com>
2021-01-09 18:08:00 -0800

    ev3remote class

--- ev3remote.py
+++ ev3remote.py
@@ -1,10 +1,17 @@
 from paramiko import SSHClient, AutoAddPolicy
 import config # store username/password in local file
+# https://www.kite.com/python/answers/how-to-ssh-using-paramiko-in-python
 
-client = SSHClient()
-client.load_system_host_keys()
-client.load_host_keys('~/.ssh/known_hosts')
-client.set_missing_host_key_policy(AutoAddPolicy())
+class ev3remote:
 
-client.connect(config.MACHINE, config.USERNAME, config.PASSWORD)
-client.close()
+    def __init__(self):
+        client = SSHClient()
+	client.load_system_host_keys()
+	client.load_host_keys('~/.ssh/known_hosts')
+	client.set_missing_host_key_policy(AutoAddPolicy())
+	client.connect(config.MACHINE, config.USERNAME, config.PASSWORD)
+
+	client.close()
+
+if __name__ == '__main__':
+
