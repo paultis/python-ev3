36adb741aef3078095ec1f23d9da467e6ca3b585
paultis <11512940+paultis@users.noreply.github.com>
2021-01-09 20:10:06 -0800

    remote works

--- ev3remote.py
+++ ev3remote.py
@@ -1,4 +1,4 @@
-from paramiko import SSHClient, AutoAddPolicy
+from paramiko import SSHClient
 import config # store username/password in local file
 # https://www.kite.com/python/answers/how-to-ssh-using-paramiko-in-python
 
@@ -7,9 +7,10 @@ class ev3remote:
     def __init__(self, machine, username, password):
         self.client = SSHClient()
         self.client.load_system_host_keys()
-        self.client.load_host_keys('~/.ssh/known_hosts')
-        self.client.set_missing_host_key_policy(AutoAddPolicy())
-        self.client.connect(machine, username, password)
+        # self.client.load_host_keys('~/.ssh/known_hosts')
+        # self.client.set_missing_host_key_policy(AutoAddPolicy())
+        print(machine + ' ' + username + ' ' + password)
+        self.client.connect(machine, username=username, password=password)
 
     def close(self):
         self.client.close()
@@ -21,7 +22,9 @@ class ev3remote:
 
 if __name__ == '__main__':
     ev3 = ev3remote(config.MACHINE, config.USERNAME, config.PASSWORD)
-    lines = ev3.run_command('ls')
+    lines = ev3.run_command('(cd /home/robot/python-ev3; pwd)')
+    print(lines)
+    lines = ev3.run_command("python3 /home/robot/python-ev3/ev3command.py -c 'on_for_rotations' -d 'neg' -s 5. -o 'D' -r 0.2")
     print(lines)
     ev3.close()
 
